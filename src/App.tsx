f49d2c4fc7c28f35700a9647e4e889024d45eebe
TraviSS-0 <146763084+TraviSS-0@users.noreply.github.com>
2023-11-11 11:23:27 +0100

    Zod dodany, działa

--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,118 @@
 import { useForm } from "react-hook-form";
 import "./style.css";
 import { useRef, useState } from "react";
-import { DevTool } from "@hookform/devtools";
+import { ZodType, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  password: string;
+  confirmPassword: string;
+  email: string;
+  pesel: string;
+};
 
 export default function App() {
-  type FormValues = {
-    fname: string;
-    lname: string;
-    password: string;
-    confirmpassword: string;
-    email: string;
-    pesel: number;
-  };
+  const schema: ZodType<FormValues> = z
+    .object({
+      firstName: z
+        .string()
+        .min(3, { message: "Imię musi składać się z minimum 3 liter!" }),
+      lastName: z
+        .string()
+        .min(3, { message: "Nazwisko musi się składać z minimum 3 liter!" }),
+      password: z
+        .string()
+        .min(6, { message: "Hasło musi się składać z minimum 6 znaków!" })
+        .max(30, { message: "Hasło nie może być dłuże niż 30 znaków!" }),
+      confirmPassword: z
+        .string()
+        .min(6, { message: "Hasło musi się składać z minimum 6 znaków!" })
+        .max(30, { message: "Hasło nie może być dłuże niż 30 znaków!" }),
+      email: z.string().email({ message: "Niepoprawny format email!" }),
+      pesel: z
+        .string()
+        .length(11, { message: "Pesel się składa z 11 cyfr!" })
+        .refine((value: string) => /^\d+$/.test(value), {
+          message: "Pesel musi składać się z samych cyfr!",
+        }),
+    })
 
-  const form = useForm<FormValues>({
-    defaultValues: {
-      fname: "",
-      lname: "",
-      password: "",
-      confirmpassword: "",
-      email: "",
-      pesel: 0,
-    },
-  });
-  const { register, control, handleSubmit, formState, watch, getValues } = form;
-  const { errors } = formState;
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Hasła do siebie nie pasują",
+      path: ["confirmPassword"],
+    });
 
-  // const userFirstName = watch("fname");
-  // const userLastName = watch("lname");
-  // const userPassword = watch("password");
-  // const userEmail = watch("email");
-  // const userPesel = watch("pesel");
+  const submitData = (data: FormValues) => {
+    const userFirstName = data.firstName;
+    const userLastName = data.lastName;
+    const userPassword = data.password;
+    const userEmail = data.email;
+    const userPesel = +data.pesel;
 
-  const password = useRef({});
-  password.current = watch("password", "");
-
-  const onSubmit = (data: FormValues) => {
     console.log("Form submitted", data);
+    console.log("User First Name:", userFirstName);
+    console.log("User Last Name:", userLastName);
+    console.log("User Password:", userPassword);
+    console.log("User Email:", userEmail);
+    console.log("User Pesel:", userPesel);
   };
 
-  // const handleGetValues = () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: zodResolver(schema) });
 
   return (
     <div className="main-flex">
       <h1>Formularz Obywatelski</h1>
-      <form id="main-form" onSubmit={handleSubmit(onSubmit)} noValidate>
+      <form onSubmit={handleSubmit(submitData)} id="main-form">
         <input
           type="text"
-          id="fname"
+          id="firstName"
           placeholder="Imię"
-          {...register("fname", {
-            required: "Imię jest wymagane",
-          })}
+          {...register("firstName")}
         />
-        <p>{errors.fname?.message}</p>
+        {errors.firstName && <p>{errors.firstName?.message}</p>}
         <input
           type="text"
-          id="lname"
+          id="lastName"
           placeholder="Nazwisko"
-          {...register("lname", {
-            required: "Nazwisko jest wymagane",
-          })}
+          {...register("lastName")}
         />
-        <p>{errors.lname?.message}</p>
+        {errors.lastName && <p>{errors.lastName?.message}</p>}
         <input
           type="text"
           id="password"
           placeholder="Hasło"
-          {...register("password", {
-            required: "Hasło jest wymagane",
-            minLength: {
-              value: 5,
-              message: "Twoje hasło musi miec minimum 5 znaków długości",
-            },
-          })}
+          {...register("password")}
         />
-        <p>{errors.password?.message}</p>
+        {errors.password && <p>{errors.password.message}</p>}
         <input
           type="text"
-          id="confirmpassword"
+          id="confirmPassword"
           placeholder="Powtórz hasło"
-          {...register("confirmpassword", {
-            required: "Powtórzenie hasła jest wymagane",
-            validate: (value) =>
-              value === password.current || "Hasła się nie zgadzają",
-          })}
+          {...register("confirmPassword")}
         />
-        <p>{errors.confirmpassword?.message}</p>
+        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
         <input
           type="text"
           id="email"
           placeholder="Twój adres email"
-          {...register("email", {
-            required: "Adres email jest wymagany",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "Invalid email adress",
-            },
-          })}
+          {...register("email")}
         />
-        <p>{errors.email?.message}</p>
+        {errors.email && <p>{errors.email.message}</p>}
         <input
-          type="number"
+          type="text"
           id="pesel"
           placeholder="Pesel"
-          {...register("pesel", {
-            required: {
-              value: true,
-              message: "Pesel jest wymagany",
-            },
-            minLength: {
-              value: 11,
-              message: "Pesel musi być długi na 11 znaków",
-            },
-            maxLength: {
-              value: 11,
-              message: "Pesel musi być długi na 11 znaków",
-            },
-
-            valueAsNumber: true,
-          })}
+          {...register("pesel")}
         />
-        <p>{errors.pesel?.message}</p>
-        <button type="button" /* onClick={handleGetValues} */>Confirm</button>
+        {errors.pesel && <p>{errors.pesel.message}</p>}
+        <button type="submit">Confirm</button>
       </form>
-      <DevTool control={control} />
     </div>
   );
 }
